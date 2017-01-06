d7ddd27d5348a97282b2b29907018ed633ab9fa6
Павел Штогренфельц < >
2017-01-06 16:02:58 +0700

    Added models for posts and comments, comment class is empty

---
+++ src/BashParserCore/Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BashParserCore.Models
+{
+    public class Post
+    {
+        public int PostID { get; set; }
+        public string Rating { get; set; }
+        public string Date { get; set; }
+        public string PostName { get; set; }
+        public string Text { get; set; }
+
+        public virtual IEnumerable<Comment> Comments { get; set; }
+    }
+}
